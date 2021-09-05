996871e8c308f23289f9186749b93af789f82c5d
yousefmamdo <yousefmando1998@gmail.com>
2021-09-05 23:32:55 +0300

    finsh lap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      latitude:'',
+      longitude:'',
+      display_name:'',
+      mapFlag:false,
+      displayErr:false
+    }
+  }
+  
+  getLocationData = async (event) => {
+    event.preventDefault();
+    const cityName = event.target.cityName.value;
+    const myKey = 'pk.a47d4213dbce9a84687e69e822dfacad';
+    const URL = `https://eu1.locationiq.com/v1/search.php?key=${myKey}&q=${cityName}&format=json`;
+    try 
+    {
+      let resResult = await axios.get(URL);
+      this.setState({
+        latitude:resResult.data[0].lat,
+        longitude:resResult.data[0].lon,
+        display_name:resResult.data[0].display_name,
+        mapFlag:true
+      })
+    }
+    catch 
+    {
+      console.log('Eror not fond');
+      this.setState({
+        displayErr:true
+      })
+    }
+
+  }
+  
+  render(){
+    return(
+      <div class="p-3 mb-2 bg-light text-dark">
+      <h1>Location App</h1>
+      <form onSubmit={this.getLocationData}  class="form-inline">
+   
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Enter city name</label >
+    <input type="text" name="cityName" class="form-control" id="exampleInputEmail1" placeholder="Enter city name"/>
+   
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Exploler</button>
+</form>
+      {/* render the data */}
+      {this.state.mapFlag &&  <p>Display name : {this.state.display_name}</p>}
+      {this.state.mapFlag &&  <p>latitude : {this.state.latitude}</p>}
+      {this.state.mapFlag &&  <p>longitude : {this.state.longitude}</p>}
+
+      {this.state.mapFlag && 
+      
+      <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.latitude},${this.state.longitude}`} class="img-thumbnail" alt='map' />}
+
+      {this.state.displayErr && <p>Sorry Error</p>}
+      
+
+      </div>
+    )
+  }
 }
 
 export default App;
